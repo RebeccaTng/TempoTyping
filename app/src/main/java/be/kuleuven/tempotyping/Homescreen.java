e4af875e293a22b917cbea54172805774c306199
rebecca.tang <rebecca.tang@student.kuleuven.be>
2021-05-04 18:36:17 +0200

    v1.5

--- app/src/main/java/be/kuleuven/tempotyping/Homescreen.java
+++ app/src/main/java/be/kuleuven/tempotyping/Homescreen.java
@@ -2,15 +2,26 @@ package be.kuleuven.tempotyping;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Homescreen extends AppCompatActivity {
+    private Button play;
+    private Button leaderboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        play = (Button) findViewById(R.id.play);
+        leaderboard = (Button) findViewById(R.id.leaderboard);
     }
 
-    //hoi enzo enzo
+    public void goPlay(View caller) {
+        Intent goToPlay = new Intent(this, Play.class);
+        startActivity(goToPlay);
+    }
 }

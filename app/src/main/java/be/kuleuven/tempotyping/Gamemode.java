6e064805b76e4ada8cf6a1103a5ca8975e10a1b1
yannick.vancamp <yannick.vancamp@student.kuleuven.be>
2021-05-20 19:32:23 +0200

    v4.3

--- app/src/main/java/be/kuleuven/tempotyping/Gamemode.java
+++ app/src/main/java/be/kuleuven/tempotyping/Gamemode.java
@@ -10,7 +10,6 @@ import android.text.InputFilter;
 import android.text.InputType;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
